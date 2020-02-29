8f3974d42f7b93af42841f498b498b52d1a5a758
Sergii Shevchenko <sergiishevchenko017@gmail.com>
2020-02-29 19:53:15 +0100

    init

--- converter/views.py
+++ converter/views.py
@@ -1,4 +1,12 @@
-from airtable import airtable
+# from airtable import airtable
+from django.shortcuts import render
+import logging
 
-at = airtable.Airtable('BASE_ID', 'API_KEY')
-at.get('TABLE_NAME')
+logger = logging.getLogger(__name__)
+
+# at = airtable.Airtable('BASE_ID', 'API_KEY')
+# at.get('TABLE_NAME')
+
+
+def init(request):
+    return render(request, 'diploma.html')
