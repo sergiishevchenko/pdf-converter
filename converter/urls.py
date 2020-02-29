59bb862a3bdcb60221a4f2b3d0cd8ebaefcb5e23
Sergii Shevchenko <sergiishevchenko017@gmail.com>
2020-02-29 19:52:23 +0100

    init

--- converter/urls.py
+++ converter/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from converter.views import init
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', init, name='init')
 ]
