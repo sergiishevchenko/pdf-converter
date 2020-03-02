4521211538b62820a8f7a5c49e34e4736ebd1034
Sergii Shevchenko <sergiishevchenko017@gmail.com>
2020-03-02 16:23:49 +0100

    add pdfkit

--- converter/views.py
+++ converter/views.py
@@ -1,9 +1,19 @@
 from airtable import Airtable
 from django.shortcuts import render
 from converter.config import base_key, api_key
+import pdfkit
 
 
 def init(request):
     airtable = Airtable(base_key, 'Table 1', api_key)
-    print(airtable.get_all())
-    return render(request, 'diploma.html')
+    for script in airtable.get_all():
+        name = script['fields']['Имя']
+        surname = script['fields']['Фамилия']
+        date = script['fields']['Дата']
+        # config = pdfkit.configuration(wkhtmltopdf="/Users/serg/Desktop/projects/pdf-converter/myenv/lib/python3.7/site-packages/pdfkit/configuration.py")
+        pdfkit.from_file('converter/templates/diploma.html', 'out.pdf')
+        params = {
+            'name': name,
+            'surname': surname,
+            'date': date}
+        return render(request, 'diploma.html', params)
