66d6d29a0766fdd41468c43fbf6eeed671ff2526
Sergii Shevchenko <sergiishevchenko017@gmail.com>
2020-03-06 16:03:54 +0100

    final commit

--- converter/views.py
+++ converter/views.py
@@ -7,28 +7,31 @@ from django.http import HttpResponse
 
 
 def init(request):
+    # connection with Airtable
     airtable = Airtable(base_key, 'Table 1', api_key)
-
+    # data processing
     template = get_template('diploma.html')
     for script in airtable.get_all():
-        name = script['fields']['Имя']
-        surname = script['fields']['Фамилия']
-        date = script['fields']['Дата']
-        html = render(request, 'diploma.html', {'name': name, 'surname': surname, 'date': date})
-        config = pdfkit.configuration(wkhtmltopdf='/usr/local/bin/wkhtmltopdf')
-        options = {
-            'dpi': 300,
-            'page-size': 'A4',
-            'orientation': 'landscape',
-            'margin-top': '0',
-            'margin-right': '0.',
-            'margin-bottom': '0.',
-            'margin-left': '0',
-            'encoding': "UTF-8",
-            'custom-header': [
-                ('Accept-Encoding', 'gzip')
-            ],
-            'no-outline': None
-        }
-        pdfkit.from_file('converter/templates/diploma.html', 'out.pdf', configuration=config, options=options)
+        if script['fields']['Допуск'] == 'True':
+            name = script['fields']['Имя']
+            surname = script['fields']['Фамилия']
+            date = script['fields']['Дата']
+            html = render(request, 'diploma.html', {'name': name, 'surname': surname, 'date': date})
+            result = (html.content).decode('utf-8')
+            config = pdfkit.configuration(wkhtmltopdf='/usr/local/bin/wkhtmltopdf')
+            options = {
+                'dpi': 300,
+                'page-size': 'A4',
+                'orientation': 'landscape',
+                'margin-top': '0',
+                'margin-right': '0.',
+                'margin-bottom': '0.',
+                'margin-left': '0',
+                'encoding': "UTF-8",
+                'custom-header': [
+                    ('Accept-Encoding', 'gzip')
+                ],
+                'no-outline': None
+            }
+            pdfkit.from_string(result, '{}-{}.pdf'.format(name, surname), configuration=config, options=options)
     return render(request, 'diploma.html')
